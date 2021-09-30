30e038ece9a477e70ba83c57a4838a867aa1aeef
Venras <venrasv@gmail.com>
2021-09-30 23:08:56 +0300

    Исправление функций

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 function getRandomIntInclusive(min, max) {
-  if (min >= 0 && max >= 0) {
+  if (min >= 0 && max >= min) {
     min = Math.ceil(min);
     max = Math.round(max);
-    return Math.round(Math.random() * (max - min + 1)) + min;
+    return Math.round(Math.random() * (max - min) + min);
   }
+  throw new Error('Ошибка. Обнаружено не соответствие условию.');
 }
+getRandomIntInclusive(1, 3);
 
-function getRandomFloat(min, max) {
-  if (min >= 0 && max >= 0) {
-    return Math.random() * (max - min) + min;
+function getRandomFloat(min, max, numb) {
+  if (min >= 0 && max >= min) {
+    return (Math.random() * (max - min + 1) + min).toFixed(numb);
   }
+  throw new Error('Ошибка. Обнаружено не соответствие условию.');
 }
+getRandomFloat(1, 3, 2);
